6257437f3815f26b3b0a66192296ffafb330fdc6
freddex <fredjuiceman@yahoo.com>
2022-05-19 17:55:49 -0400

    commit tree 3cb078d5b2d97783b6df49f43eb0097cfa6b4f5d

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch (list, x) {
+  let lengthOfName = x.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === x;
+  });
+}
+
+function matchName (list, name) {
+  return list.filter( driver => driver.name.toLowerCase() === name.toLowerCase()
+  );
+}
+
+
+
+
